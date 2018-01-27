84a5bad726d5a4f625f8bd31ddc83defa1af3aef
Ran Crump <rancrump18@gmail.com>
2018-01-27 02:02:11 -0600

    Ring Bug

---
+++ GlobalGameJam/Assets/Pinged.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinged : MonoBehaviour {
+
+    public GameObject Collider_Left, Collider_Right;
+
+    private Renderer Left, Right;
+
+    public bool isVisible = false;
+    public float timer;
+
+	void Start () {
+        /*
+        left = gameObject.GetComponent<BoxCollider>();
+        right = gameObject.GetComponent<BoxCollider>();
+        */
+
+        Left = Collider_Left.GetComponent<Renderer>();
+        Left.enabled = false;
+        Right = Collider_Right.GetComponent<Renderer>();
+        Right.enabled = false;
+    }
+	
+	
+	void Update () {
+	
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            Left.enabled = true;
+            Right.enabled = true;
+        }
+
+        if (timer <= 0)
+        {
+            Left.enabled = false;
+            Right.enabled = false;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Ring")
+        {
+            timer = 1.5f;
+        }
+    }
+}
---
+++ GlobalGameJam/Assets/Scripts/ShieldSegment.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldSegment : MonoBehaviour {
+    public ShieldRing ring;
+    public GameObject colliderLeft;
+    public GameObject colliderRight;
+    public float openTime;
+    public float closeTime;
+    public float holdOpenTime;
+    public float heldTime;
+    public enum Mode
+    {
+        None,
+        Opening,
+        HeldOpen,
+        Closing
+    }
+    public Mode mode;
+    public float amountClosed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Rotate the shield segment
+        transform.rotation *= Quaternion.Euler(0, 0, ring.rotateSpeed * Time.deltaTime);
+
+        // Update state
+        float openSpeed = Time.deltaTime / openTime;
+        float closeSpeed = Time.deltaTime / closeTime;
+        switch (mode)
+        {
+            case Mode.None:
+                break;
+            case Mode.Opening:
+                amountClosed -= openSpeed;
+                if (amountClosed < 0.0f)
+                {
+                    mode = Mode.HeldOpen;
+                    heldTime = holdOpenTime;
+                    amountClosed = 0.0f;
+                }
+                break;
+            case Mode.Closing:
+                amountClosed += closeSpeed;
+                if (amountClosed >= 1.0f)
+                {
+                    mode = Mode.None;
+                    heldTime = holdOpenTime;
+                    amountClosed = 1.0f;
+                }
+                break;
+            case Mode.HeldOpen:
+                heldTime -= Time.deltaTime;
+                if (heldTime < 0.0f)
+                {
+                    heldTime = 0.0f;
+                    mode = Mode.Closing;
+                }
+                break;
+        }
+        Vector3 v;
+        v = colliderLeft.transform.localScale;
+        v[0] = amountClosed;
+        colliderLeft.transform.localScale = v;
+        v = colliderRight.transform.localScale;
+        v[0] = amountClosed;
+        colliderRight.transform.localScale = v;
+    }
+
+    public void SetToOpening()
+    {
+        if (mode == Mode.None)
+        {
+            mode = Mode.Opening;
+        }
+    }
+}
