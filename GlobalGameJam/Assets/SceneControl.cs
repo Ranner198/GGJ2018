28f95578f1a1d823b1f9e139cf4c9436503fb4d4
Lysle Shields <lysle@lesink.com>
2018-01-27 11:40:06 -0600

    First screens

---
+++ GlobalGameJam/Assets/SceneControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour {
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void OnCredits()
+    {
+        SceneManager.LoadScene("CreditsScreen");
+    }
+
+    public void OnSettings()
+    {
+        SceneManager.LoadScene("SettingsScreen");
+    }
+
+    public void GoTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
