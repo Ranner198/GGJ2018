349396df6d3a8fc979d76fb00df8072109de5ba0
Ran Crump <rancrump18@gmail.com>
2018-01-26 20:02:28 -0600

    V0.01

---
+++ GlobalGameJam/Assets/Scripts/EnemyFlightScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlightScript : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    private Vector3 distance;
+
+    public Transform Player;
+
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+
+    void Update() {
+        float _distance = (transform.position - Player.transform.position).magnitude;
+
+        print(_distance);
+        if (_distance > 3)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, .1f);
+        }
+    }
+}
---
+++ GlobalGameJam/Assets/Scripts/FlightScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed, rotSpeed;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update () {
+        float X;
+        float Y;
+
+        X = Input.GetAxis("Horizontal");
+        Y = Input.GetAxis("Vertical");
+
+        Vector2 Vec2 = new Vector2(X * speed, Y * speed);
+
+        rb.velocity = Vec2 * Time.deltaTime;
+	}
+}
