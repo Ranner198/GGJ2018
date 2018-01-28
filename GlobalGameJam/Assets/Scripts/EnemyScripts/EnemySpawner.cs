5f8755a4117ad1d5dec2df8ba2aa57947228f732
Lysle Shields <lysle@lesink.com>
2018-01-28 11:31:25 -0600

    mothership spins from time to time

--- GlobalGameJam/Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ GlobalGameJam/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject Enemy;
+    public float shootTimeMin;
+    public float shootTimeMax;
+    public float shootTime;
+    public float geometricAvg = 0.9f;
+    public Animator anim;
+    public float lastSpeed = 1.0f;
+    public float timeUpdate = 0.0f;
+
+    private float timeToShoot;
 
 	void Start () {
         InvokeRepeating("SpawnEnemies", 6, 6);
 	}
+
+    void ResetShootTimer()
+    {
+        shootTime = Random.Range(shootTimeMin, shootTimeMax);
+    }
 
     void SpawnEnemies()
     {
@@ -16,5 +30,38 @@ public class EnemySpawner : MonoBehaviour {
         {
             Instantiate(Enemy, transform.position, transform.rotation);
         }
+    }
+
+    private void Update()
+    {
+        float targetSpeed = anim.speed;
+
+        shootTime -= Time.deltaTime;
+        if (shootTime <= 0)
+        {
+            // Time out!
+            ResetShootTimer();
+        }
+
+        if (shootTime < 3.0f)
+        {
+            if (shootTime < 1.0f)
+            {
+                targetSpeed = 10.0f;
+            } else
+            {
+                targetSpeed = 3.0f / shootTime;
+            }
+        } else
+        {
+            targetSpeed = 1.0f;
+        }
+        timeUpdate += Time.deltaTime;
+        lastSpeed = anim.speed;
+        while (timeUpdate > 0.1f) {
+            lastSpeed = lastSpeed * geometricAvg + targetSpeed * (1-geometricAvg);
+            timeUpdate -= 0.1f;
+        }
+        anim.speed = lastSpeed;
     }
 }
