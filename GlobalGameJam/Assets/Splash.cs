67c8ec30ae911de8d77b5628cff20941b141b0b8
Lysle Shields <lysle@lesink.com>
2018-01-28 13:10:18 -0600

    Added splash screen for GGJ

---
+++ GlobalGameJam/Assets/Splash.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Splash : MonoBehaviour {
+    public RectTransform rt;
+    public float startScale = 0.45f;
+    public float endScale = 0.50f;
+    public float timeLimit = 1.2f;
+    private float timeTaken = 0;
+    public Image image;
+      
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeTaken += Time.deltaTime;
+        if (timeTaken < 0.5f)
+        {
+            Color c = image.color;
+            c.a = timeTaken * 2;
+            image.color = c;
+        }
+        if (Input.anyKey || timeTaken >= timeLimit)
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+        float v = (endScale - startScale) * (timeTaken / timeLimit) + startScale;
+        rt.localScale = new Vector2(v, v);
+    }
+}
