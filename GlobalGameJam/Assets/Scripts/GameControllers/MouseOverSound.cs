ea2c803812e6519d29a101dcafd4e18ec67818f0
Ran Crump <rancrump18@gmail.com>
2018-01-28 15:30:56 -0600

    Final

---
+++ GlobalGameJam/Assets/Scripts/GameControllers/MouseOverSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverSound : MonoBehaviour {
+
+    public AudioClip MouseOver, MouseClick;
+
+    public AudioSource source;
+
+    public bool Over;
+
+    public void OnMouseEnter() {
+        source.PlayOneShot(MouseOver);
+    }
+
+    public void OnMouseExit()
+    {
+        Over = false;
+    }
+}
