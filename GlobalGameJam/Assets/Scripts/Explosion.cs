4e08235c9281c9c27ece91363073046bd2d0061a
Ran Crump <rancrump18@gmail.com>
2018-01-27 19:40:19 -0600

    Repush

---
+++ GlobalGameJam/Assets/Scripts/Explosion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public int health = 100;
+
+    public float force;
+
+    public Renderer Player;
+
+    public ParticleSystem PS;
+
+    void Start()
+    {
+        PS.enableEmission = false;
+    }
+
+    void FixedUpdate () {
+
+        if (health <= 0)
+        {
+            Player.enabled = false;
+            PS.enableEmission = true;
+            GameManager.hasLost = true;           
+        }
+	}
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (gameObject.tag == "Bullet")
+        {
+            health -= 25;
+        }
+    }
+}
